c9bb4759072475d916b47ea5a8c0afdb4f441df0
CL3-PC02 <CL3-PC02@DESKTOP-TSV6U2V>
2025-07-11 17:17:44 +0800

    SampleProgram ACT - 2E

---
+++ src/main/java/com/mycompany/yoshisample/YoshiSample.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.yoshisample;
+
+/**
+ *
+ * @author CL3-PC02
+ */
+public class YoshiSample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Iriz Dizon");
+    }
+}
